8a96accc99307732bbd70debf872637d7418c494
lurongze <1946755280@qq.com>
2020-04-30 18:12:59 +0800

    button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,10 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import './index.scss'
+
+export default ()=>{
+
+  return (
+    <View>123</View>
+  )
+}
