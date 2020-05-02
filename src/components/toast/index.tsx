f0bd2c1bdcaf97012716df24d40cd63eff045522
1946755280@qq.com <SA523BERxia$meng>
2020-05-02 22:42:22 +0800

    点击toast消失

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -14,12 +14,17 @@ interface propsType{
 }
 
 export default (props: propsType) => {
-  const { text='',icon='', during=2, show=false, onHide } = props;
+  const { text='',icon='', during=2, show=false, tapClose = false, onHide } = props;
   const handleHide = ()=>{
+    if(tapClose){
+      onHide && onHide();
+    }
+  }
+  const animationEnd = ()=>{
     onHide && onHide();
   }
   return (
-    <View onAnimationEnd={()=>handleHide()} style={{animationDuration: during+'s'}} className={`toast-mask mask-base ${show?'toastAnimate':''}`}>
+    <View onAnimationEnd={()=>animationEnd()} onClick={()=>handleHide()} style={{animationDuration: during+'s'}} className={`toast-mask mask-base ${show?'toastAnimate':''}`}>
       <View className='toast'>
         {icon && (<View className='icon'>icon</View>)}
         <View>{text}</View>
